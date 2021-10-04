b9458ae5e380c771aa840b0911db365a4426cc2e
Dominique Eav <dominique.eav@blue-mind.net>
2021-10-04 23:10:11 +0200

    Overhaul for more consistency + better Eb treble key bass part export

--- process-score.py
+++ process-score.py
@@ -8,65 +8,129 @@ import argparse
 
 # Inspired from https://musescore.org/en/node/287888
 
-parser = argparse.ArgumentParser(description="create PDF parts for C, Bb, Eb instruments")
-parser.add_argument('musescore_file', type=str)
+parser = argparse.ArgumentParser(
+    description="create PDF parts for C, Bb, Eb instruments"
+)
+parser.add_argument("musescore_file", type=str)
 args = parser.parse_args()
 
-MUSE_APP = '/home/dom/Apps/MuseScore-3.6.2.548021370-x86_64.AppImage'
+MUSE_APP = "/home/dom/Apps/MuseScore-3.6.2.548021370-x86_64.AppImage"
 
-print('Creating output folders')
-subprocess.run(['mkdir', '-p', 'C', 'Bb', 'Eb'])
+keys = ("C", "Bb", "Eb")
 
-print('Executing MuseScore...')
-out = subprocess.run([MUSE_APP, args.musescore_file, '--score-parts'], capture_output=True)
-print(out.stderr.decode('utf-8'))
+print("Creating output folders")
+subprocess.run(["mkdir", "-p", *keys])
+
+print("Executing MuseScore...")
+out = subprocess.run(
+    [MUSE_APP, args.musescore_file, "--score-parts"], capture_output=True
+)
+print(out.stderr.decode("utf-8"))
 result = json.loads(out.stdout)
-for part, content in zip(result['parts'], result['partsBin']):
-    # C
-    filepath_C = f"C/{ part.replace(' ', '_') }_C.mscz"
-    with open(filepath_C, 'wb') as outfile:
-        print('Writing', filepath_C)
-        outfile.write(base64.b64decode(content))
-    filepath_C_pdf = f"C/{ part.replace(' ', '_') }_C.pdf"
-    subprocess.run([MUSE_APP, filepath_C, '-o', filepath_C_pdf], capture_output=False)    
+for part, content in zip(result["parts"], result["partsBin"]):
 
-    # C bass Clef
-    filepath_C_bassClef = f"C/{ part.replace(' ', '_') }_C_bassClef.mscx"
-    out = subprocess.run(['unzip', '-p', filepath_C, '*.msc*'], capture_output=True)
-    bassClef = 'F15ma'
-    if part.lower().startswith('bass'):
-        bassClef = 'F8va'
-    c_bass_file = out.stdout.decode('utf-8')
-    if 'transposingClefType' in c_bass_file:
-        c_bass_file = c_bass_file.replace('<transposingClefType>G</transposingClefType>', f'<transposingClefType>{ bassClef }</transposingClefType>')
-    else:
-        c_bass_file = c_bass_file.replace('<voice>', f'<voice><Clef><concertClefType>G</concertClefType><transposingClefType>{ bassClef }</transposingClefType></Clef>', 1)
-    with open(filepath_C_bassClef, 'w') as outfile:
-        print('Writing', filepath_C_bassClef)
-        outfile.write(c_bass_file)
-    filepath_C_bassClef_pdf = f"C/{ part.replace(' ', '_') }_C_bassClef.pdf"
-    subprocess.run([MUSE_APP, filepath_C_bassClef, '-o', filepath_C_bassClef_pdf], capture_output=False)
-
-    # Bb
-    transposeConfig = '{"mode": "by_interval", "direction": "up", "transposeInterval": 4, "transposeKeySignatures": true}'
-    out = subprocess.run([MUSE_APP, filepath_C, '--score-transpose', transposeConfig], capture_output=True)
-    content = json.loads(out.stdout)['mscz']
-    filepath_Bb = f"Bb/{ part.replace(' ', '_') }_Bb.mscz"
-    with open(filepath_Bb, 'wb') as outfile:
-        print('Writing', filepath_Bb)
+    part = part.replace(" ", "_")
+    filepath_C_mscz = f"C/tmp_{part}_C.mscz"
+    with open(filepath_C_mscz, "wb") as outfile:
+        print("Writing", filepath_C_mscz)
         outfile.write(base64.b64decode(content))
-    filepath_Bb_pdf = f"Bb/{ part.replace(' ', '_') }_Bb.pdf"
-    subprocess.run([MUSE_APP, filepath_Bb, '-o', filepath_Bb_pdf, '-p', 'tag_Bb.qml'], capture_output=False)
 
-    # Eb
-    transposeConfig = '{"mode": "by_interval", "direction": "down", "transposeInterval": 7, "transposeKeySignatures": true}'
-    out = subprocess.run([MUSE_APP, filepath_C, '--score-transpose', transposeConfig], capture_output=True)
-    content = json.loads(out.stdout)['mscz']
-    filepath_Eb = f"Eb/{ part.replace(' ', '_') }_Eb.mscz"
-    with open(filepath_Eb, 'wb') as outfile:
-        print('Writing', filepath_Eb)
-        outfile.write(base64.b64decode(content))
-    filepath_Eb_pdf = f"Eb/{ part.replace(' ', '_') }_Eb.pdf"
-    subprocess.run([MUSE_APP, filepath_Eb, '-o', filepath_Eb_pdf, '-p', 'tag_Eb.qml'], capture_output=False)
-    
-    subprocess.run(['rm', filepath_C, filepath_C_bassClef, filepath_Bb, filepath_Eb])
+    for key in keys:
+        if key == "C":
+            filepath_mscz = filepath_C_mscz
+        else:
+            if key == "Bb":
+                transposeConfig = '{"mode": "by_interval", "direction": "up", "transposeInterval": 4, "transposeKeySignatures": true}'
+            elif key == "Eb":
+                transposeConfig = '{"mode": "by_interval", "direction": "down", "transposeInterval": 7, "transposeKeySignatures": true}'
+            else:
+                raise Exception(f"Unknown key [{key}]?!?")
+            out = subprocess.run(
+                [MUSE_APP, filepath_C_mscz, "--score-transpose", transposeConfig],
+                capture_output=True,
+            )
+            content = json.loads(out.stdout)["mscz"]
+            filepath_mscz = f"{key}/tmp_{part}_{key}.mscz"
+            with open(filepath_mscz, "wb") as outfile:
+                print("Writing", filepath_mscz)
+                outfile.write(base64.b64decode(content))
+
+        # direct PDF
+        filepath_direct = f"{key}/{part}_{key}.pdf"
+        subprocess.run(
+            [MUSE_APP, filepath_mscz, "-o", filepath_direct, "-p", f"tag_{key}.qml"],
+            capture_output=False,
+        )
+
+        if part.lower().startswith("bass"):
+            # treble Clef, for people who can't read bass key
+            filepath_trebleClef = f"{key}/tmp_{part}_{key}_trebleClef.mscx"
+            out = subprocess.run(
+                ["unzip", "-p", filepath_mscz, "*.msc*"], capture_output=True
+            )
+            treble_file = out.stdout.decode("utf-8")
+            if key == "Eb":
+                transposingClefType = "G15mb"
+            else:
+                transposingClefType = "G8vb"
+            if "transposingClefType" in treble_file:
+                treble_file = treble_file.replace(
+                    "<transposingClefType>F</transposingClefType>",
+                    f"<transposingClefType>{transposingClefType}</transposingClefType>",
+                )
+            else:
+                treble_file = treble_file.replace(
+                    "<voice>",
+                    f"<voice><Clef><concertClefType>F</concertClefType><transposingClefType>{transposingClefType}</transposingClefType></Clef>",
+                    1,
+                )
+            with open(filepath_trebleClef, "w") as outfile:
+                print("Writing", filepath_trebleClef)
+                outfile.write(treble_file)
+            filepath_trebleClef_pdf = f"{key}/{part}_{key}_trebleClef.pdf"
+            subprocess.run(
+                [
+                    MUSE_APP,
+                    filepath_trebleClef,
+                    "-o",
+                    filepath_trebleClef_pdf,
+                    f"tag_{key}.qml",
+                ],
+                capture_output=False,
+            )
+
+        else:
+            # bass Clef, for trombone
+            filepath_bassClef = f"{key}/tmp_{part}_{key}_bassClef.mscx"
+            out = subprocess.run(
+                ["unzip", "-p", filepath_mscz, "*.msc*"], capture_output=True
+            )
+            bass_file = out.stdout.decode("utf-8")
+            if "transposingClefType" in bass_file:
+                bass_file = bass_file.replace(
+                    "<transposingClefType>G</transposingClefType>",
+                    f"<transposingClefType>F15ma</transposingClefType>",
+                )
+            else:
+                bass_file = bass_file.replace(
+                    "<voice>",
+                    f"<voice><Clef><concertClefType>G</concertClefType><transposingClefType>F15ma</transposingClefType></Clef>",
+                    1,
+                )
+            with open(filepath_bassClef, "w") as outfile:
+                print("Writing", filepath_bassClef)
+                outfile.write(bass_file)
+            filepath_bassClef_pdf = f"{key}/{part}_{key}_bassClef.pdf"
+            subprocess.run(
+                [
+                    MUSE_APP,
+                    filepath_bassClef,
+                    "-o",
+                    filepath_bassClef_pdf,
+                    f"tag_{key}.qml",
+                ],
+                capture_output=False,
+            )
+
+    for key in keys:
+        subprocess.run(f"rm {key}/tmp_*", shell=True)
