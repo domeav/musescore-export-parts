fa14858b8ca17b717f7cd5638765cbc66dfafb7f
Dominique Eav <dominique.eav@bluemind.net>
2024-12-04 11:24:58 +0100

    Replaced original script

--- MuseScoreWrapper.py
+++ MuseScoreWrapper.py
@@ -14,60 +14,110 @@ MUSE_APP = "/home/dom/Apps/MuseScore-Studio-Nightly-latest-master-x86_64.AppImag
 TRANSPOSITIONS = {
     "C": None,
     "Bb": '{"mode": "by_interval", "direction": "up", "transposeInterval": 4, "transposeKeySignatures": true}',
-    "Eb": '{"mode": "by_interval", "direction": "down", "transposeInterval": 7, "transposeKeySignatures": true}'
+    "Eb": '{"mode": "by_interval", "direction": "down", "transposeInterval": 7, "transposeKeySignatures": true}',
 }
 
-CLEF_TABLE = ['15mb', '8vb', '', '8va', '15ma']
 
-
-class MuseScoreWrapper():
-    def __init__(self, mscz_path):
+class MuseScoreWrapper:
+    # clef must be None for conductor scores
+    def __init__(self, mscz_path, clef, key):
+        self.clef = clef
+        self.key = key
         if mscz_path:
             self.tmp_dir = TemporaryDirectory(delete=True)
             self.tmp_path = Path(self.tmp_dir.name)
-            with zipfile.ZipFile(mscz_path, 'r') as mscz:
-                mscx_filenames = [f for f in mscz.namelist() if f.endswith('.mscx')]
+            with zipfile.ZipFile(mscz_path, "r") as mscz:
+                mscx_filenames = [f for f in mscz.namelist() if f.endswith(".mscx")]
                 mscz.extractall(self.tmp_path)
-        assert(len(mscx_filenames) == 1)
+        assert len(mscx_filenames) == 1
         self.mscx_name = mscx_filenames[0]
         self.mscx_path = self.tmp_path / self.mscx_name
         self.mscx = ET.parse(self.mscx_path)
-        self.title = self.mscx.find('./Score/Staff/VBox/Text/text').text
+        self.title = self.mscx.find("./Score/Staff/VBox/Text/text").text
         self._sanitize()
 
     def _sanitize(self):
-        for part in self.mscx.findall('./Score/Part'):
-            partName = part.find('./Instrument/longName').text
-            part.find('./trackName').text = partName
-            part.find('./Instrument/trackName').text = partName
+        for part in self.mscx.findall("./Score/Part"):
+            partName = part.find("./Instrument/longName").text
+            part.find("./trackName").text = partName
+            part.find("./Instrument/trackName").text = partName
         self.mscx.write(self.mscx_path)
+
     def _generate_mscz(self, tmp_out):
         out_path = Path(tmp_out.name)
         tmp_filename = str(uuid.uuid4())
-        shutil.make_archive(out_path / tmp_filename, 'zip', self.tmp_path)
-        shutil.move(out_path / f'{tmp_filename}.zip', out_path / f'{tmp_filename}.mscz')
-        return out_path / f'{tmp_filename}.mscz'
+        shutil.make_archive(out_path / tmp_filename, "zip", self.tmp_path)
+        shutil.move(out_path / f"{tmp_filename}.zip", out_path / f"{tmp_filename}.mscz")
+        return out_path / f"{tmp_filename}.mscz"
+
     def generate_pdf(self, pdf_path):
+        print(f"Generating {pdf_path}")
         tmp_out = TemporaryDirectory(delete=True)
         mscz_path = self._generate_mscz(tmp_out)
-        subprocess.run(
-            [MUSE_APP, mscz_path, "-o", pdf_path],
-            capture_output=False
-        )
+        subprocess.run([MUSE_APP, mscz_path, "-o", pdf_path], capture_output=True)
+
     def set_title(self, title):
-        self.mscx.find('./Score/Staff/VBox/Text/text').text = title
+        self.mscx.find("./Score/Staff/VBox/Text/text").text = title
         self.mscx.write(self.mscx_path)
+        self.title = title
+
     def transpose(self, key):
         tmp_out = TemporaryDirectory(delete=True)
         mscz_path = self._generate_mscz(tmp_out)
         out = subprocess.run(
-            [MUSE_APP,
-             mscz_path,
-             "--score-transpose",
-             TRANSPOSITIONS[key]],
+            [MUSE_APP, mscz_path, "--score-transpose", TRANSPOSITIONS[key]],
             capture_output=True,
         )
         content = base64.b64decode(json.loads(out.stdout)["mscz"])
-        return MuseScoreWrapper(io.BytesIO(content))
-        
+        return MuseScoreWrapper(io.BytesIO(content), clef=self.clef, key=key)
 
+    def _switch_clef(self):
+        CLEF_TABLE = ["15mb", "8vb", "", "8va", "15ma"]
+        if not self.clef:
+            raise ValueException
+        target_clef = "F" if self.clef == "G" else "G"
+        if target_clef == "G":
+            clef_qualifier_index = 1
+        elif target_clef == "F":
+            clef_qualifier_index = -2 if self.key == "Eb" else -1
+        tmp_out = TemporaryDirectory(delete=True)
+        mscz_path = self._generate_mscz(tmp_out)
+        switched_part = MuseScoreWrapper(mscz_path, clef=target_clef, key=self.key)
+        clefInfo = switched_part.mscx.find("./Score/Staff/Measure/voice/Clef")
+        parent = switched_part.mscx.find("./Score/Staff/Measure/voice")
+        if clefInfo:
+            parent.remove(clefInfo)
+        parent.insert(
+            0,
+            ET.XML(
+                f"""
+        <Clef>
+            <concertClefType>{target_clef}</concertClefType>
+            <transposingClefType>{target_clef}{CLEF_TABLE[clef_qualifier_index]}</transposingClefType>
+            <isHeader>1</isHeader>
+        </Clef>
+        """
+            ),
+        )
+        switched_part.mscx.write(switched_part.mscx_path)
+        return switched_part
+
+    def generate_parts(self):
+        print(f"Generating parts for {self.title}")
+        tmp_out = TemporaryDirectory(delete=True)
+        mscz_path = self._generate_mscz(tmp_out)
+        out = subprocess.run(
+            [MUSE_APP, mscz_path, "--score-parts"], capture_output=True
+        )
+        result = json.loads(out.stdout)
+        parts = {}
+        for part, content in zip(result["parts"], result["partsBin"]):
+            part = part.replace(" ", "_")
+            clef = ("G", "F")
+            if part.lower().startswith("bass"):
+                clef = ("F", "G")
+            parts[(part, clef[0])] = MuseScoreWrapper(
+                io.BytesIO(base64.b64decode(content)), clef=clef[0], key=self.key
+            )
+            parts[(part, clef[1])] = parts[(part, clef[0])]._switch_clef()
+        return parts
--- process-score.py
+++ process-score.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import sys
-import json
-import base64
-import subprocess
+
 import argparse
-import re
+from MuseScoreWrapper import MuseScoreWrapper
+import os
+import concurrent.futures
+
 
 # Inspired from https://musescore.org/en/node/287888
 
@@ -15,103 +15,18 @@ parser = argparse.ArgumentParser(
 parser.add_argument("musescore_file", type=str)
 args = parser.parse_args()
 
-MUSE_APP = "/home/dom/Apps/MuseScore-3.6.2.548021370-x86_64.AppImage"
-
-keys = ("C", "Bb", "Eb")
-CLEF_TABLE = ['15mb', '8vb', '', '8va', '15ma']
-
-
-print("\n### Creating output folders")
-subprocess.run(["mkdir", "-p", *keys])
-
-print("\n### Transposing conductors...")
-for key in keys:
-    if key == "C":
-        subprocess.run(["cp", args.musescore_file, "C/tmp_conductor.mscz"], capture_output=False)
-        continue
-    elif key == "Bb":
-        transposeConfig = '{"mode": "by_interval", "direction": "up", "transposeInterval": 4, "transposeKeySignatures": true}'
-    elif key == "Eb":
-        transposeConfig = '{"mode": "by_interval", "direction": "down", "transposeInterval": 7, "transposeKeySignatures": true}'
-    else:
-        raise Exception(f"Unknown key [{key}]?!?")
-    out = subprocess.run(
-        [MUSE_APP, args.musescore_file, "--score-transpose", transposeConfig],
-        capture_output=True,
-    )    
-    content = json.loads(out.stdout)["mscz"]
-    with open(f"{key}/tmp_conductor.mscz", "wb") as outfile:
-        outfile.write(base64.b64decode(content))
-
-print("\n### Generating conductors scores...")
-for key in keys:
-    subprocess.run(
-        [MUSE_APP, f"{key}/tmp_conductor.mscz", "-o", f"{key}/conductor.pdf"],
-        capture_output=False,
-   )
-
-def _generate_pdf(contents, target_file, key):
-    'Generate normal and pocket pdfs from in-memory mscx'
-    with open(f"tmp.mscx", "w") as outfile:
-            outfile.write(mscx_contents)
-    subprocess.run(
-        [MUSE_APP, "tmp.mscx", "-o", f'{target_file}.pdf', "-p", f"tag_{key}.qml"],
-        capture_output=False,
-    )
-    # pocket version
-    pocket_mscx = re.sub('<Spatium>[^<]+</Spatium>', '<Spatium>2.9</Spatium>', mscx_contents)
-    with open(f"tmp.mscx", "w") as outfile:
-            outfile.write(pocket_mscx)
-    subprocess.run(
-        [MUSE_APP, "tmp.mscx", "-o", f'{target_file}_pocket.pdf', "-p", f"tag_{key}.qml"],
-        capture_output=False,
-    )
-
-    
-print("Fetching parts...")
-for key in keys:
-    out = subprocess.run(
-        [MUSE_APP, f"{key}/tmp_conductor.mscz", "--score-parts"], capture_output=True
-    )
-    print(out.stderr.decode("utf-8"))
-    result = json.loads(out.stdout)
-    for part, content in zip(result["parts"], result["partsBin"]):
-        part = part.replace(" ", "_")
-        with open(f"{key}/tmp_{part}.mscz", "wb") as outfile:
-            outfile.write(base64.b64decode(content))
-        out = subprocess.run(
-            ["unzip", "-p", f"{key}/tmp_{part}.mscz", "*.msc*"], capture_output=True
-        )
-        mscx_contents = out.stdout.decode("utf-8")
-        m = re.search('<transposingClefType>([^<]+)</transposingClefType>', mscx_contents)
-        if m:
-            clef, qualifier = m.group(1)[0], m.group(1)[1:]
-        elif part.lower().startswith('bass'):
-            clef = 'F'
-        else:
-            clef = 'G'
-        clef_qualifier_index = CLEF_TABLE.index(clef[1:])
-        _generate_pdf(mscx_contents, f"{key}/{part}_{clef}", key)
-        # switch clef
-        if clef == 'F':
-            transposing_clef = 'G'
-            target_qualifier_index = -2 if key == 'Eb' else -1
-            target_qualifier_index += clef_qualifier_index
-        elif clef == 'G':
-            transposing_clef = 'F'
-            target_qualifier_index = clef_qualifier_index + 1
-        if "transposingClefType" in mscx_contents:
-            mscx_contents = mscx_contents.replace(
-                f"<transposingClefType>{clef}{qualifier}</transposingClefType>",
-                f"<transposingClefType>{transposing_clef}{CLEF_TABLE[target_qualifier_index]}</transposingClefType>")
-        else:
-            mscx_contents = mscx_contents.replace(
-                "<voice>",
-                f"<voice><Clef><concertClefType>{clef}</concertClefType><transposingClefType>{transposing_clef}{CLEF_TABLE[target_qualifier_index]}</transposingClefType></Clef>",
-                1)
-        _generate_pdf(mscx_contents, f"{key}/{part}_{transposing_clef}", key)
-
-
-for key in keys:
-    subprocess.run(f"rm {key}/tmp_*", shell=True)
-subprocess.run(f"rm tmp.mscx", shell=True)
+conductors = {}
+conductors["C"] = MuseScoreWrapper(args.musescore_file, clef=None, key="C")
+conductors["Bb"] = conductors["C"].transpose("Bb")
+conductors["Eb"] = conductors["C"].transpose("Eb")
+with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+    for key in conductors.keys():
+        try:
+            os.mkdir(key)
+        except FileExistsError:
+            pass
+        conductors[key].set_title(conductors[key].title + f" ({key})")
+        parts = conductors[key].generate_parts()
+        for part, msw in parts.items():
+            executor.submit(msw.generate_pdf, f"{key}/{part[0]}_{part[1]}.pdf")
+        executor.submit(conductors[key].generate_pdf, f"{key}/conductor.pdf")
